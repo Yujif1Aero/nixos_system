c909c3f5c5d9e7f51cda70f1393265d56e4f3d9d
Yujif1Aero <yuji.shimojima.s@gmail.com>
2025-01-06 16:15:30 +0100

    add aditional stoping suto suspend

--- configuration.nix
+++ configuration.nix
@@ -71,8 +71,9 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
   #SMJM edited
- #  services.gnome.gnome-remote-desktop.enable = true;
+ 
    services.xserver.desktopManager.gnome = {
     enable = true;
     extraGSettingsOverrides = ''
